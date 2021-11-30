1b61ddd801ba8f65f2998b3cd152f0ce38a31e11
Talen Fisher <talen.fisher@cythral.com>
2021-11-30 14:43:17 -0600

    BRGD-201 Deploy Static Assets Pipeline Stack

---
+++ cicd/scripts/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cwd=$(dirname "${BASH_SOURCE[0]}")
+bin=$cwd/../../bin
+artifact_store=$1
+
+create_infrastructure_config_files()
+{
+    for file in $(ls ${cwd}/../stacks/brighid-infrastructure/params/*.json); do
+        envName=$(echo $file | xargs basename | sed "s/\.json//")
+        params=$(cat $file)
+        
+        config=$(cat $cwd/../stacks/brighid-infrastructure/config.json)
+        config=$(echo $config | jq --argjson params "$params" '.Parameters=$params')
+        echo $config > $bin/brighid-infrastructure.${envName}.config.json
+    done
+}
+
+package_static_assets_pipeline()
+{
+    sam package \
+        --template-file $cwd/../stacks/static-assets-pipeline/template.yml \
+        --s3-bucket $artifact_store \
+        --output-template-file $bin/static-assets-pipeline.template.yml
+}
+
+
+mkdir -p $bin
+
+create_infrastructure_config_files
+package_static_assets_pipeline
+cp $cwd/../stacks/brighid-infrastructure/template.yml $bin/brighid-infrastructure.template.yml
+cp $cwd/../stacks/dns/template.yml $bin/dns.template.yml
---
+++ cicd/scripts/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cwd=$(dirname ${BASH_SOURCE[0]:-0})
+bin=$cwd/../../bin
+rm -rf $bin
